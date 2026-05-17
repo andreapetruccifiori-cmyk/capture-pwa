8931f972b986ccfe6e6bdafcf28e722af973f182
andreapetruccifiori-cmyk <andreapetruccifiori@gmail.com>
2026-05-17 13:06:26 +0200

    fix: bump service worker to v2, clear all caches on activate

--- sw.js
+++ sw.js
@@ -1,2 +1,8 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+const CACHE_VERSION = 'v2';
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.delete(CACHE_VERSION));
+});
+self.addEventListener('activate', (e) => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())
+));
